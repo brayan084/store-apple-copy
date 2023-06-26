332e21006c963a11f73caee32ce034c095ba49ef
Josefina <giangualanoj@gmail.com>
2023-06-26 19:45:17 -0300

    First commit

---
+++ tp/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+import 'primeicons/primeicons.css';
+import "primeflex/primeflex.css";
+
+
+function App() {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default App;
