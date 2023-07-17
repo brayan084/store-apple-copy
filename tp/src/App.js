c095e58fc781daae4a23b58ec40d1c065e990b3f
Josefina <giangualanoj@gmail.com>
2023-07-17 18:46:54 -0300

    commit

--- tp/src/App.js
+++ tp/src/App.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import './App.css';
-import "primereact/resources/themes/lara-light-indigo/theme.css";
-import 'primeicons/primeicons.css';
-import "primeflex/primeflex.css";
 
 
 function App() {
