e97e46e261b28551b479a0a017f791d1262e715b
brayan <zorrobrayan0@gmail.com>
2023-07-25 17:17:38 -0300

    .

---
+++ tp/src/pantalla-producto.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react';
+import { Galleria } from 'primereact/galleria';
+import { PhotoService } from './PhotoService';
+import "./pantalla_producto.css"
+
+export default function BasicDemo() {
+    const [images, setImages] = useState(null);
+    const responsiveOptions = [
+        {
+            breakpoint: '991px',
+            numVisible: 4
+        },
+        {
+            breakpoint: '767px',
+            numVisible: 3
+        },
+        {
+            breakpoint: '575px',
+            numVisible: 1
+        }
+    ];
+
+    useEffect(() => {
+        PhotoService.getImages().then(data => setImages(data));
+    }, [])
+
+    const itemTemplate = (item) => {
+        return <img src={item.itemImageSrc} alt={item.alt} style={{ width: '100%' }} />
+    }
+
+    const thumbnailTemplate = (item) => {
+        return <img src={item.thumbnailImageSrc} alt={item.alt} />
+    }
+
+    return (
+        <div className="card">
+            <Galleria value={images} responsiveOptions={responsiveOptions} numVisible={3} style={{ maxWidth: '540px' }} 
+                item={itemTemplate} thumbnail={thumbnailTemplate} />
+        </div>
+    )
+}
+        
