b4db8096d1da60941c0fa57f51577be6290308a2
brayan <zorrobrayan0@gmail.com>
2023-08-08 03:15:04 -0300

    ultimo cambio de lugar de css en reportes

--- tp/src/pantallas de productos/pantalla del producto imac/etiquetas.js
+++ tp/src/pantallas de productos/pantalla del producto imac/etiquetas.js
@@ -5,9 +5,7 @@ import { Chip } from 'primereact/chip';
 export default function Etiquetas() {
     return (
         <div className="flex flex-wrap gap-2">
-            <Chip label="NUEVO" />
-            <Chip label="ENVIO GRATIS" />
-            <Chip label="STOCK DISPONIBLE" />
+            <Chip label="SIN STOCK DISPONIBLE" />
         </div>
     );
 }
--- tp/src/reportes/reportes_ventas.js
+++ tp/src/reportes/reportes_ventas.js
@@ -1,5 +1,4 @@
 import { Bar } from 'react-chartjs-2';
-import "./reportes.css";
 import {
     Chart as ChartJS,
     CategoryScale,
